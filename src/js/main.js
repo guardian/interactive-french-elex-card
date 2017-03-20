71c67b551d3cdd16aad92853d59766c3ecba5df2
cfen <chris.fenn@guardian.co.uk>
2017-03-20 15:07:42 +0000

    updated s3 address

--- src/js/main.js
+++ src/js/main.js
@@ -23,18 +23,18 @@ export function init(el, context, config, mediator) {
         url: urlStr,
         type: 'json',
         crossOrigin: true,
-        success: (resp) => buildEditView(resp)//el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.cards}`
+        success: (resp) => buildEditView(resp) //el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.cards}`
     });
 
     [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
         var network = shareEl.getAttribute('data-network');
-        shareEl.addEventListener('click',() => shareFn(network));
+        shareEl.addEventListener('click', () => shareFn(network));
     });
 }
 
 
 
-function buildEditView(d){
+function buildEditView(d) {
 
     //selectedItem =='Houseprices' ? selectedItem = selectedItemConst : selectedItem = selectedItem;
 
@@ -44,7 +44,7 @@ function buildEditView(d){
 
     d = formatData(d);
 
-    let editHtml = Mustache.render(cardsHTML, d );
+    let editHtml = Mustache.render(cardsHTML, d);
     console.log(d);
     document.querySelector('.test-msg').innerHTML = `${editHtml}`;
 }
@@ -64,21 +64,18 @@ function formatData(data) {
         card.PolicyThreeCopy = whitespaceFixOne(card.PolicyThreeCopy);
         card.PolicyFourCopy = whitespaceFixOne(card.PolicyFourCopy);
         card.PolicyFiveCopy = whitespaceFixOne(card.PolicyFiveCopy);
-        if(card.ShortLeaderRef == selectedItem){ card.valid=true; };
+        if (card.ShortLeaderRef == selectedItem) { card.valid = true; };
 
         return card;
-        
+
     });
 
+    data.cards.map((card) => {
+        if (selectedItem == card.ShortLeaderRef) { newObj.cards.push(card); }
 
-    data.cards.map((card) => {    
-        if(selectedItem==card.ShortLeaderRef){ newObj.cards.push(card); }
-        
     });
 
-
-    newObj.selectedCandidate = data.selectedCandidate=selectedItem;
-
+    newObj.selectedCandidate = data.selectedCandidate = selectedItem;
 
     return newObj;
 }
@@ -97,61 +94,61 @@ function whitespaceFixRemoveSpaceAndAccents(s) {
 }
 
 
-function convert_accented_characters(str){
-        var conversions = new Object();
-        conversions['ae'] = 'ä|æ|ǽ';
-        conversions['oe'] = 'ö|œ';
-        conversions['ue'] = 'ü';
-        conversions['Ae'] = 'Ä';
-        conversions['Ue'] = 'Ü';
-        conversions['Oe'] = 'Ö';
-        conversions['A'] = 'À|Á|Â|Ã|Ä|Å|Ǻ|Ā|Ă|Ą|Ǎ';
-        conversions['a'] = 'à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª';
-        conversions['C'] = 'Ç|Ć|Ĉ|Ċ|Č';
-        conversions['c'] = 'ç|ć|ĉ|ċ|č';
-        conversions['D'] = 'Ð|Ď|Đ';
-        conversions['d'] = 'ð|ď|đ';
-        conversions['E'] = 'È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě';
-        conversions['e'] = 'è|é|ê|ë|ē|ĕ|ė|ę|ě';
-        conversions['G'] = 'Ĝ|Ğ|Ġ|Ģ';
-        conversions['g'] = 'ĝ|ğ|ġ|ģ';
-        conversions['H'] = 'Ĥ|Ħ';
-        conversions['h'] = 'ĥ|ħ';
-        conversions['I'] = 'Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ';
-        conversions['i'] = 'ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı';
-        conversions['J'] = 'Ĵ';
-        conversions['j'] = 'ĵ';
-        conversions['K'] = 'Ķ';
-        conversions['k'] = 'ķ';
-        conversions['L'] = 'Ĺ|Ļ|Ľ|Ŀ|Ł';
-        conversions['l'] = 'ĺ|ļ|ľ|ŀ|ł';
-        conversions['N'] = 'Ñ|Ń|Ņ|Ň';
-        conversions['n'] = 'ñ|ń|ņ|ň|ŉ';
-        conversions['O'] = 'Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ';
-        conversions['o'] = 'ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º';
-        conversions['R'] = 'Ŕ|Ŗ|Ř';
-        conversions['r'] = 'ŕ|ŗ|ř';
-        conversions['S'] = 'Ś|Ŝ|Ş|Š';
-        conversions['s'] = 'ś|ŝ|ş|š|ſ';
-        conversions['T'] = 'Ţ|Ť|Ŧ';
-        conversions['t'] = 'ţ|ť|ŧ';
-        conversions['U'] = 'Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ';
-        conversions['u'] = 'ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ';
-        conversions['Y'] = 'Ý|Ÿ|Ŷ';
-        conversions['y'] = 'ý|ÿ|ŷ';
-        conversions['W'] = 'Ŵ';
-        conversions['w'] = 'ŵ';
-        conversions['Z'] = 'Ź|Ż|Ž';
-        conversions['z'] = 'ź|ż|ž';
-        conversions['AE'] = 'Æ|Ǽ';
-        conversions['ss'] = 'ß';
-        conversions['IJ'] = 'Ĳ';
-        conversions['ij'] = 'ĳ';
-        conversions['OE'] = 'Œ';
-        conversions['f'] = 'ƒ';
-        for(var i in conversions){
-            var re = new RegExp(conversions[i],"g");
-            str = str.replace(re,i);
-        }
-        return str;
+function convert_accented_characters(str) {
+    var conversions = new Object();
+    conversions['ae'] = 'ä|æ|ǽ';
+    conversions['oe'] = 'ö|œ';
+    conversions['ue'] = 'ü';
+    conversions['Ae'] = 'Ä';
+    conversions['Ue'] = 'Ü';
+    conversions['Oe'] = 'Ö';
+    conversions['A'] = 'À|Á|Â|Ã|Ä|Å|Ǻ|Ā|Ă|Ą|Ǎ';
+    conversions['a'] = 'à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª';
+    conversions['C'] = 'Ç|Ć|Ĉ|Ċ|Č';
+    conversions['c'] = 'ç|ć|ĉ|ċ|č';
+    conversions['D'] = 'Ð|Ď|Đ';
+    conversions['d'] = 'ð|ď|đ';
+    conversions['E'] = 'È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě';
+    conversions['e'] = 'è|é|ê|ë|ē|ĕ|ė|ę|ě';
+    conversions['G'] = 'Ĝ|Ğ|Ġ|Ģ';
+    conversions['g'] = 'ĝ|ğ|ġ|ģ';
+    conversions['H'] = 'Ĥ|Ħ';
+    conversions['h'] = 'ĥ|ħ';
+    conversions['I'] = 'Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ';
+    conversions['i'] = 'ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı';
+    conversions['J'] = 'Ĵ';
+    conversions['j'] = 'ĵ';
+    conversions['K'] = 'Ķ';
+    conversions['k'] = 'ķ';
+    conversions['L'] = 'Ĺ|Ļ|Ľ|Ŀ|Ł';
+    conversions['l'] = 'ĺ|ļ|ľ|ŀ|ł';
+    conversions['N'] = 'Ñ|Ń|Ņ|Ň';
+    conversions['n'] = 'ñ|ń|ņ|ň|ŉ';
+    conversions['O'] = 'Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ';
+    conversions['o'] = 'ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º';
+    conversions['R'] = 'Ŕ|Ŗ|Ř';
+    conversions['r'] = 'ŕ|ŗ|ř';
+    conversions['S'] = 'Ś|Ŝ|Ş|Š';
+    conversions['s'] = 'ś|ŝ|ş|š|ſ';
+    conversions['T'] = 'Ţ|Ť|Ŧ';
+    conversions['t'] = 'ţ|ť|ŧ';
+    conversions['U'] = 'Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ';
+    conversions['u'] = 'ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ';
+    conversions['Y'] = 'Ý|Ÿ|Ŷ';
+    conversions['y'] = 'ý|ÿ|ŷ';
+    conversions['W'] = 'Ŵ';
+    conversions['w'] = 'ŵ';
+    conversions['Z'] = 'Ź|Ż|Ž';
+    conversions['z'] = 'ź|ż|ž';
+    conversions['AE'] = 'Æ|Ǽ';
+    conversions['ss'] = 'ß';
+    conversions['IJ'] = 'Ĳ';
+    conversions['ij'] = 'ĳ';
+    conversions['OE'] = 'Œ';
+    conversions['f'] = 'ƒ';
+    for (var i in conversions) {
+        var re = new RegExp(conversions[i], "g");
+        str = str.replace(re, i);
+    }
+    return str;
 }
